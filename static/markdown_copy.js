ded50755e9cccc60c3455b80370fd7f487db3c8d
raiseyang <y22222ly@163.com>
2022-01-26 15:24:07 +0800

    update from gridea: 2022-01-26 15:24:07

--- static/markdown_copy.js
+++ static/markdown_copy.js
@@ -1,60 +1,125 @@
 // ==UserScript==
 // @name         复制成 Markdown 的 Link
 // @namespace    https://raiseyang.github.io/
-// @version      0.1
+// @version      0.3.0
 // @description  将当前网站链接拷贝成 Markdown 格式
 // @author       RaiseYang
 // @include        *
 // @match        https://blog.csdn.net/**
-// @icon         https://raiseyang.github.io/raiseyang/markdown_copy.png
+// @match        https://juejin.cn/**
+// @match        https://developer.android.google.cn/**
+// @match        https://github.com/**
+// @match        https://www.jianshu.com/**
+// @match        https://cloud.tencent.com/**
+// @match        https://medium.com/**
+// @exclude      /(^[^:\/#\?]*:\/\/([^#\?\/]*\.)?www\.baidu\.com(:[0-9]{1,5})?\/.*$)/
+// @exclude      /(^[^:\/#\?]*:\/\/([^#\?\/]*\.)?www\.google\.com\.hk\/.*$/
+// @icon         https://raiseyang.github.io/static/markdown_copy.png
+// @updateURL    https://raiseyang.github.io/static/markdown_copy.js
 // @grant        GM_setClipboard
 // @require     https://cdn.bootcss.com/jquery/2.1.2/jquery.min.js
 // @license        MIT
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
 
     // Your code here...
-      var $ = window.$;
+    var $ = window.$;
 
     console.log('复制成 Markdown 的 Link 脚本加载了');
-	var button = document.createElement("button"); //创建一个input对象（提示框按钮）
-	button.id = "id001";
-	button.textContent = "Markdown Link";
-    button.style.background="red";
-	button.style.width = "100px";
-	button.style.height = "40px";
-	button.style.position = "fixed";
-	button.style.bottom = "100px";
-	button.style.right = "20px";
+    var button = document.createElement("button"); //创建一个input对象（提示框按钮）
+    button.id = "id001";
+    button.textContent = "拷贝链接";
+    button.style.cssText = `
+            position: fixed;
+            bottom: 100px;
+            right: 10px;
+            background:#282C34;
+            width: 92px;
+            height: 30px;
+            color:white;
+            z-index:999;
+            font-weight:bold;
+            font-size:15px;
+            border-radius:50px;
+            `;
 
-	//绑定按键点击功能
-	button.onclick = function (){
-		console.log('点击了按键');
+    //绑定按键点击功能
+    button.onclick = function () {
+        console.log('点击了按键');
 
         // 当前网站链接
         var link = window.location.href
 
-        let title;
+            let title;
 
-        let CSDNegexp= /.*csdn\.net\/.+/; // 匹配CSDN
-        if(CSDNegexp.test(link)){
+        let CSDNegexp = /.*csdn\.net\/.+/; // 匹配CSDN
+        let Juejinegexp = /.*juejin\.cn\/.+/; // 匹配掘金
+        let Androidegexp = /developer\.android\.google\.cn.+/;
+        let GitHubegexp = /github\.com.+/;
+        let JianShuegexp = /.*jianshu\.com.+/;
+        let TencentCloudegexp = /cloud.tencent\.com.+/;
+        let Mediumegexp = /medium\.com.+/;
+
+        if (CSDNegexp.test(link)) {
             console.log("CSDNegexp")
 
             title = $("#articleContentId").text(); // CSDN的title定制化
-        }else {
+        } else if (Juejinegexp.test(link)) {
+            console.log("Juejinegexp")
+            title = $(".article-title").text().trim(); // 掘金的title定制化
+        } else if (Androidegexp.test(link)) {
+            console.log("Androidegexp")
+            title = $(document).attr('title');
+            //button.style.right = 100;//按钮左移100px不起作用
+        } else if (GitHubegexp.test(link)) {
+            console.log("GitHubegexp")
+            title = $(document).attr('title'); //android/gradle-recipes: Ready-to-use recipes for common build customizations that showcase the Android Gradle plugin's public APIs and DSL.
+            let endIndex = title.indexOf(":");
+            title = title.substr(0, endIndex);
+
+        } else if (JianShuegexp.test(link)) {
+            console.log("JianShuegexp")
+            title = $(document).attr('title'); //Kevin Learn Android：Android 知识点梳理 - 简书
+            let endIndex = title.indexOf(" - 简书");
+            title = title.substr(0, endIndex);
+        } else if (TencentCloudegexp.test(link)) {
+            console.log("TencentCloudegexp")
+            title = $(document).attr('title'); //OkHttp使用完全教程 - 云+社区 - 腾讯云
+            let endIndex = title.indexOf(" - 云+社区 - 腾讯云");
+            title = title.substr(0, endIndex);
+        } else if (Mediumegexp.test(link)) {
+            console.log("Mediumegexp")
+            title = getMeta("og:title");
+
+        } else {
             console.log("other site")
             title = $(document).attr('title');
         }
 
-		//alert("["+title+"]("+link+")");
-        let markdownLink = "["+title+"]("+link+")"; // 拼接成Markdown格式
+        //alert("["+title+"]("+link+")");
+        let markdownLink = "[" + title + "](" + link + ")"; // 拼接成Markdown格式
         GM_setClipboard(markdownLink, "text"); // 复制到剪切板
-        console.log('GM_setClipboard='+markdownLink); // 控制台打印，便于调试
-		return;
-	};
+        console.log('GM_setClipboard=' + markdownLink); // 控制台打印，便于调试
+        return;
+    };
 
-    $("body").append(button); // 添加button按钮
+    setTimeout(() => {
+        $("body").append(button); // 添加button按钮
+    }, 1000);
+
+	// 获取<meta>信息 <meta data-rh="true" property="og:title" content="Room 🔗 Flow">
+    function getMeta(metaName) {
+        const metas = document.getElementsByTagName("meta");
+
+        for (let i = 0; i < metas.length; i++) {
+            if (metas[i].getAttribute("property") === metaName) {
+                return metas[i].getAttribute("content");
+            }
+        }
+
+        return;
+    }
 
 })();
